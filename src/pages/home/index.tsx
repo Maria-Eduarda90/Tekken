626e366b880710f772b2e51a76c462e09dbbcc30
Mary Skywalker <snowdennaruto90@gmail.com>
2023-02-12 20:51:46 -0400

    finished home screen

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,14 @@
-import styles from '.styles.module.scss';
+import styles from './styles.module.scss';
+import { Input } from '../../components/Input';
+import logo from '../../img/Tekken_Logo.png';
 
 export function Home(){
     return(
-        <h1></h1>
+        <div className={styles.container}>
+            <img src={logo} alt="logo" />
+            <form>
+                <Input />
+            </form>
+        </div>
     );
 }
