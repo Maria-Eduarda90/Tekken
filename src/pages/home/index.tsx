c8d9636d3b1cce8e9cc26d7606d5afe33c214ecb
Mary Skywalker <snowdennaruto90@gmail.com>
2023-02-12 21:25:11 -0400

    signIn function

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,30 @@
 import styles from './styles.module.scss';
 import { Input } from '../../components/Input';
 import logo from '../../img/Tekken_Logo.png';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function Home(){
+    const [password, setPassword] = useState<string>();
+    const navigate = useNavigate();
+
+    function handleSignIn(e: FormEvent){
+        e.preventDefault();
+
+        if (password === "Tekken123"){
+            navigate('/characters');
+        } else if (password !== "Tekken123"){
+            alert("senha errada.")
+        } else {
+            navigate('/');
+        }
+    }
+
     return(
         <div className={styles.container}>
             <img src={logo} alt="logo" />
-            <form>
-                <Input />
+            <form onSubmit={handleSignIn}>
+                <Input value={password} onChange={e => setPassword(e.target.value)} />
             </form>
         </div>
     );
