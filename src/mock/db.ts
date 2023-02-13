ca08fad50a317ab0d2c7f90cd2877022d525911e
Mary Skywalker <snowdennaruto90@gmail.com>
2023-02-13 19:57:40 -0400

    characters added

---
+++ src/mock/db.ts
@@ -0,0 +1,111 @@
+import alisaAvatar from '../img/alisa.jpg';
+import alisaImage from "../img/Alisa-Bosconovitch.png";
+import HeihachiAvatar from "../img/heihachi.jpg";
+import HeihachiImage from "../img/Heihachi-Mishima.png";
+import JinAvatar from "../img/jin.jpg";
+import JinImage from "../img/Jin-Kazama.png";
+import LarsAvatar from "../img/lars.jpg";
+import LarsImage from "../img/Lars-Alexanderson.png";
+import LiliAvatar from "../img/lili.jpg";
+import LiliImage from "../img/Lili-De-Rochefort.png";
+import PaulAvatar from "../img/paul.jpg";
+import PaulImage from "../img/Paul-Phoenix.png";
+import AsukaAvatar from "../img/asuka.jpg";
+import AsukaImage from "../img/Asuka-Kazama.png";
+import MiguelAvatar from "../img/miguel.jpg";
+import MiguelImage from "../img/Miguel-Caballero-Rojo.png";
+import SteveAvatar from "../img/steve.jpg";
+import SteveImage from "../img/Steve-Fox.png";
+
+export const characters = [
+  {
+    id: "1",
+    avatar: `${alisaAvatar}`,
+    characteristics: {
+      image: `${alisaImage}`,
+      name: "Alisa Bosconovitch",
+      description:
+        "A Alisa é um robô criado pelo Dr. Bosconovitch, daí o seu apelido. A sua aparência é baseada na filha do cientista prodígio, já falecida. Possui emoções, uma personalidade muito afável e um desejo de ajudar os seus amigos e quem estiver em risco.",
+    },
+  },
+  {
+    id: "2",
+    avatar: `${HeihachiAvatar}`,
+    characteristics: {
+      image: `${HeihachiImage}`,
+      name: "Heihachi Mishima",
+      description:
+        "Heihachi é um monstro da ofensiva. Trazendo as vantagens do estilo Mishima, tem um excelente controlo do espaço com o EWGF, lows dolorosos, jab strings fortíssimas e um wavedash que lhe permite encurtar a distância entre ele e o oponente e pressioná-lo.",
+    },
+  },
+  {
+    id: "3",
+    avatar: `${JinAvatar}`,
+    characteristics: {
+      image: `${JinImage}`,
+      name: "Jin Kazama",
+      description:
+        "Jin Kazama foi introduzido no Tekken 3 como o filho de Kazuya Mishima e Jun Kazama, tendo aprendido Mishima Style Fighting Karate com um toque de Kazama Style Traditional Martial Arts, tendo herdado ambas de Heihachi e Jun, respetivamente.",
+    },
+  },
+  {
+    id: "4",
+    avatar: `${LarsAvatar}`,
+    characteristics: {
+      image: `${LarsImage}`,
+      name: "Lars Alexandersson",
+      description:
+        "Lars é o ex-comandante da Tekken Force e líder de rebeldes contra a Mishima Zaibatsu. Utiliza artes marciais da Tekken Force. Lars foi introduzido no Tekken 6: Bloodline Rebellion.",
+    },
+  },
+  {
+    id: "5",
+    avatar: `${LiliAvatar}`,
+    characteristics: {
+      image: `${LiliImage}`,
+      name: "Emilie 'Lili' De Rochefort",
+      description:
+        "A Lili é uma personagem relativamente fácil e intuitiva. Introduzida no Tekken 5: Dark Ressurection e fazendo uso do seu estilo livre de luta que combina dança e ginástica, Lili pretende vingar o seu pai.",
+    },
+  },
+  {
+    id: "6",
+    avatar: `${PaulAvatar}`,
+    characteristics: {
+      image: `${PaulImage}`,
+      name: "Paul Phoenix",
+      description:
+        "Introduzido no primeiro Tekken e presente em todos os seguintes jogos da série, Paul está determinado a provar a todos que é o mais forte em todo o universo, tendo estado perto de ganhar o King of the Iron Fist Tournament em diversas ocasiões.",
+    },
+  },
+  {
+    id: "7",
+    avatar: `${AsukaAvatar}`,
+    characteristics: {
+      image: `${AsukaImage}`,
+      name: "Asuka Kazama",
+      description:
+        "Asuka é uma personagem que foi introduzida no Tekken 5, e faz uso das Kazama Style Traditional Martial Arts para vingar o seu pai, que foi derrotado por Feng Wei.",
+    },
+  },
+  {
+    id: "8",
+    avatar: `${MiguelAvatar}`,
+    characteristics: {
+      image: `${MiguelImage}`,
+      name: "Miguel Caballero Rojo",
+      description:
+        "O Miguel é uma personagem com um estilo próprio de luta de rua para descarregar a sua raiva nos adversários. Foi introduzido pela primeira vez no Tekken 6 para se vingar de Jin Kazama pela morte da sua irmã.",
+    },
+  },
+  {
+    id: "9",
+    avatar: `${SteveAvatar}`,
+    characteristics: {
+      image: `${SteveImage}`,
+      name: "Steve Fox",
+      description:
+        "O filho biológico de Nina Williams concebido em laboratório, Steve foi introduzido no Tekken 4 e esteve presente em todos os jogos seguintes como um pugilista britânico com um passado repleto de mistério.",
+    },
+  },
+];
